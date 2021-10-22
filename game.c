17235f59a283fb70fabb138fc6bdec5e0d4c515f
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-21 22:12:55 -0400

    Create game.c

---
+++ game.c
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Lily Liu
+// Created on: Nov 2021
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+
+    printf("Hello, World!");
+
+}
