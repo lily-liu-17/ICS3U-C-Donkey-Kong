a3294eb09cc3e72386ea9632375e9bf20dfdc7ec
lily-liu-17 <90275723+lily-liu-17@users.noreply.github.com>
2021-10-25 21:20:06 -0400

    2

--- game.c
+++ game.c
@@ -7,8 +7,13 @@
 #include <stdio.h>
 #include <gb/gb.h>
 
+#include "smiler.c"
+
 int main() {
+    // this function displays a happy face
 
-    printf("Hello, World!");
-
+    set_sprite_data(0, 2, smiler);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
 }
